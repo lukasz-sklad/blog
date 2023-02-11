9211e36f1944510444bf82d99ff1dee57e3b7679
lukasz-sklad <lukasz.sklad@gmail.com>
2023-02-11 22:57:05 +0100

    hotjar and ternary

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
+
+
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(3339999, 6)
+  }, [])
   return (
     <>
       <span className="theme-bejamas" />
