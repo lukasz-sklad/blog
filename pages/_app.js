b8de9a62aa7a2c958d2b92ec342424a61fe5f87d
lukasz-sklad <lukasz.sklad@gmail.com>
2024-06-08 20:34:09 +0200

    htjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,43 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
-import { hotjar } from 'react-hotjar'
 import { useEffect } from 'react'
+import { hotjar } from 'react-hotjar'
 
 
+
+
+// function MyApp({ Component, pageProps }) {
+//   useEffect(() => {
+//     hotjar.initialize(3339999, 6)
+//   }, [])
+//   return (
+//     <>
+//       <span className="theme-bejamas" />
+//       <Component {...pageProps} />
+//     </>
+//   );
+// }
+
+// export default MyApp;
+
+
+
+
+// import { hotjar } from 'react-hotjar';
+
+const HOTJAR_ID = 3339999; // Twoje rzeczywiste ID Hotjar
+const HOTJAR_VERSION = 6;  // Twoja wersja Hotjar
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    hotjar.initialize(3339999, 6)
-  }, [])
+    hotjar.initialize(HOTJAR_ID, HOTJAR_VERSION);
+  }, []);
+
   return (
     <>
-      <span className="theme-bejamas" />
+      
       <Component {...pageProps} />
+
     </>
   );
 }
