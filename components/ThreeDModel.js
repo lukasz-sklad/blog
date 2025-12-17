3d73c17057b8a33d037fe5e113e2fd3f1643800f
Lucas Kalinowski <lukasz.sklad@gmail.com>
2025-12-17 20:32:13 +0100

    system -plugin^netlify

--- components/ThreeDModel.js
+++ components/ThreeDModel.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ThreeDModel = ({ src, alt }) => {
   return (
-    <div className="w-full h-96 my-8 bg-gray-800 rounded-lg overflow-hidden border border-gray-700 shadow-lg">
+    <div className="w-full h-96 my-8 bg-white dark:bg-gray-800 rounded-lg overflow-hidden border border-gray-200 dark:border-gray-700 shadow-lg">
       <model-viewer
         src={src}
         alt={alt}
