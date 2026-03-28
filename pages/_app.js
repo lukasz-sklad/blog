332fc4674aa6ad270c4e699008ce0f5da6927455
Lucas Kalinowski <lukasz.sklad@gmail.com>
2026-03-29 00:52:35 +0100

    hotjar2026

--- pages/_app.js
+++ pages/_app.js
@@ -3,24 +3,11 @@ import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import { useEffect } from 'react';
 import Head from 'next/head';
-import { hotjar } from 'react-hotjar';
 import EchoTerminal from '../components/EchoTerminal';
 import Script from 'next/script';
 import ChristmasMood from '../components/ChristmasMood';
 
-const HOTJAR_ID = 3339999; // rzeczywiste ID Hotjar
-const HOTJAR_VERSION = 6; // wersja Hotjar
-
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    console.log('Inicjalizacja Hotjar');
-    hotjar.initialize(HOTJAR_ID, HOTJAR_VERSION);
-    console.log('Hotjar zainicjalizowany');
-    console.log('HOTJAR_ID:', HOTJAR_ID);
-    console.log('HOTJAR_SV:', HOTJAR_VERSION);
-    hotjar.initialize(HOTJAR_ID, HOTJAR_VERSION);
-  }, []);
-
   return (
     <>
       <Head>
