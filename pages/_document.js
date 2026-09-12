71075310483354c81afcac794450a1fcc439f80d
Lucas <lukasz.sklad@gmail.com>
2026-09-12 20:11:25 +0200

    onion

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
       <Html lang="en" className="theme-compiled">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <meta httpEquiv="onion-location" content="http://lmkvo2end2krq5ckqguwhxrr2fk6vtzlrcf42soxs6n3flsjc52co3id.onion" />
           {/* Hotjar Tracking Code for https://www.lmk.one */}
           <script
             dangerouslySetInnerHTML={{
